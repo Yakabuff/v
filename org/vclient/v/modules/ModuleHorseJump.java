9a768efc7c529d762c7fbe768f530ed156901e98
x0XP <xzeroxp@hotmail.co.uk>
2014-08-31 21:29:33 +0100

    HorseJump
    Max jump height everytime when jumping with a horse.

---
+++ org/vclient/v/modules/ModuleHorseJump.java
@@ -0,0 +1,44 @@
+package org.vclient.v.modules;
+
+import net.minecraft.client.Minecraft;
+
+import org.vclient.v.Command;
+import org.vclient.v.Vapid;
+
+public class ModuleHorseJump extends ModuleBase 
+{
+	
+	public ModuleHorseJump(Vapid vapid, Minecraft mc) 
+	{
+		super(vapid, mc);
+		// TODO Auto-generated constructor stub
+					
+		this.needsTick = true;
+		this.aliases.add("horsejump");
+		this.aliases.add("horse");
+		this.aliases.add("hj");
+		this.name = "HorseJump";	
+		this.command = new Command(this.vapid, this, aliases, "Max jump height everytime when jumping with a horse.");
+	}
+	
+	@Override
+	public void onEnable()
+	{
+		this.isEnabled = true;
+	}
+
+	@Override
+	public void onDisable()
+	{
+		this.isEnabled = false;
+
+	}
+	
+	@Override
+	public void onTick()
+	{
+		if(this.isEnabled) {
+			mc.thePlayer.horseJumpPower = 1.0F;
+		}
+	}
+}
