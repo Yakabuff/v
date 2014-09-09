e41e9d88809b23a850d085255a6f79d35dd5064a
Tristan Gosselin-Hane <starcraft66@gmail.com>
2014-09-08 20:32:07 -0400

    Change all references of Vapid to V

---
+++ org/vclient/v/StaticV.java
@@ -0,0 +1,16 @@
+package org.vclient.v;
+
+import net.minecraft.client.Minecraft;
+
+public class StaticV {
+
+	public static V V;
+	public static Minecraft mc;
+	
+	public StaticV(V V, Minecraft mc)
+	{
+		this.V = V;
+		this.mc = mc;
+	}
+
+}
