0cc05a869fa0b727a9c4554dbd4001bfec5b03b1
italiceyeball <harrymacmillan@gmail.com>
2014-09-26 07:15:15 +0100

    Changes color of name in crypto chat
    Usually the user's name is white, however I have missed messages a couple of times with it being like this. Changing it from white to light blue makes the messages more noticeable. The message text stays the same, only the user's name color is changed.

--- org/vclient/v/modules/ModuleEncryption.java
+++ org/vclient/v/modules/ModuleEncryption.java
@@ -312,7 +312,7 @@ public class ModuleEncryption extends ModuleBase
 	      try
 	      {
 	        String str3 = DecryptData(DecodeBase64(str1));
-	        str3 = "\247l" + username + ": \247r" + str3 + "\247r";
+	        str3 = "\247b\247l" + username + ": \247r" + str3 + "\247r";
 	        a.put(username, "");
 	        
 	        V.message(str3);
